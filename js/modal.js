3a62593661fb5051acf4e6f3d7e061133ba28302
Nadiia Butenko <butenko261190@gmail.com>
2026-05-13 21:21:33 +0200

    add mobile menu & add adaptive styles & update assets

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,11 @@
 const heroBtn = document.querySelector(".hero-btn")
-const overlay = document.querySelector(".overlay")
 const modalCloseBtn = document.querySelector(".modal-close-btn")
 
 heroBtn.addEventListener('click', () => {
-    overlay.classList.add('is-open')
+    document.body.classList.add('is-modal-open')
 })
 
 modalCloseBtn.addEventListener('click', () => {
-     overlay.classList.remove('is-open')
+     document.body.classList.remove('is-modal-open')
 })
 
